b6f0c46977f3e5f2309b99f2d8a7f0f30bac8196
Alok-2003 <alok953280@gmail.com>
2024-07-15 18:47:19 +0530

    done

---
+++ src/components/common-layout/index.js
@@ -0,0 +1,8 @@
+function CommonLayout({children}) {
+    return <div className="mx-auto max-w-7xl p-6 lg:px-8" >
+        {/* Header Component */}
+
+        {/* Main Component */}
+        <main>{children}</main>
+    </div>
+}
